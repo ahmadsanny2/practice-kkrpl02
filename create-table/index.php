cc89458bd7cd68a2e2b197b0f73868725718dff9
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-10-27 20:36:10 +0700

    exercise create table in PHP

---
+++ create-table/index.php
@@ -0,0 +1,30 @@
+<?php
+ob_start();
+
+$host = "127.0.0.1";
+$username = "admin";
+$password = "admin"; 
+$dbname = "mahasiswa";
+
+$connection = mysqli_connect($host, $username, $password) or die("Can't connect to MySQL server");
+
+mysqli_select_db($connection, $dbname) or die("Can't select database");
+
+$query = "CREATE TABLE mhs (
+    nim VARCHAR(10) PRIMARY KEY NOT NULL,
+    nama VARCHAR(50) NOT NULL,
+    tanggal_lahir DATE,
+    alamat TEXT
+)";
+
+$result = mysqli_query($connection, $query);
+
+if ($result) {
+    echo "Membuat tabel sukses!";
+} else {
+    echo "Gagal membuat tabel: " . mysqli_error($connection);
+}
+
+mysqli_close($connection);
+ob_end_flush();
+?>
